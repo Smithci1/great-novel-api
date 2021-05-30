d8ad1719d420430ab78a11e08ad2c67fafe72b8e
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-05-30 18:17:01 -0400

    added all up tables, to migrations

--- migrations/20210526183818-initial-table-creation.js
+++ migrations/20210526183818-initial-table-creation.js
@@ -1,37 +1,48 @@
-'use strict';
+'use strict'
 
-const { authors } = require("../models");
+const { authors } = require('../models')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     */
-    await queryInterface.createTable('authors', { 
-      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-    firstName: { type: Sequelize.STRING, allowNull: false },
-    lastName: { type: Sequelize.STRING, allowNull: false },
-    createdAt: {type: Sequelize.DATE, defaulValue: Sequelize.literal('CURRENT_TIMESTAMP')},
-    upstatedAt: {
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
-    },
-      deletedAt: { type: Sequelize.DATE}, 
-})
 
- return queryInterface.createTable.('books', {
-   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-   authorId: { type:Sequelize.INTEGER, references: { model: authors, key: 'id'}},
-   title: { type: Sequelize.STRING, allowNull:false},
-   cratedAt: {tyupe: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')},
-   updatedAt: {
-     type: Sequelize.DATE,
-    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')},
-    deletedAt: {type: Sequelize.DATE},
+    await queryInterface.createTable('authors', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      firstName: { type: Sequelize.STRING, allowNull: false },
+      lastName: { type: Sequelize.STRING, allowNull: false },
+      createdAt: { type: Sequelize.DATE, defaulValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      upstatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      },
+      deletedAt: { type: Sequelize.DATE },
+    })
+
+    await queryInterface.createTable('genres', { 
+      id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKEY: true},
+      genre: {type: Sequelize.STRING, allowNull: false},
+      createdAt: {type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')},
+      updatedAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATED CURRENT_TIMESTAMP')},
+      deletedAt: { type: Sequelize.DATE },
+    })
+    await queryInterface.createTable('books', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      authorId: { type: Sequelize.INTEGER, references: { model: authors, key: 'id' } },
+      title: { type: Sequelize.STRING, allowNull: false },
+      cratedAt: { tyupe: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      },
+      deletedAt: { type: Sequelize.DATE },
+    })
+  
+  },
+ 
+
+    return quetyInterface.createTable('bookGenres' {
+       genreid: { type: Sequelize.INTEGER, references: { model: genres, key: 'id'}},
+       bookid: {type: Sequelize.INTEGER, references: {model: books, key: 'id'}},
  })
-},
 
   down: async (queryInterface, Sequelize) => {
     /**
@@ -41,4 +52,4 @@ module.exports = {
      * await queryInterface.dropTable('users');
      */
   }
-};
+}
--- migrations/20210526190747-initial-table-creation.js
+++ migrations/20210526190747-initial-table-creation.js
@@ -1,21 +1,27 @@
 'use strict';
 
+const { genres } = require("../models");
+
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface, Sequelize, genres, books) => {
+    await queryInterface.createTable('genres', { 
+      id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKEY: true},
+      genre: {type: Sequelize.STRING, allowNull: false},
+      createdAt: {type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')},
+      updatedAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATED CURRENT_TIMESTAMP')},
+      deletedAt: { type: Sequelize.DATE },
+    })
+
+    return quetyInterface.createTable('bookGenres' {
+       genreid: { type: Sequelize.INTEGER, references: { model: genres, key: 'id'}},
+       bookid: {type: Sequelize.INTEGER, references: {model: books, key: 'id'}},
+ })
+
+    
+ 
+     
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
-};
+    
+}
